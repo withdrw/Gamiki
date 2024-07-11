287264b635c0b9a533bbfa4c57049e6c4b8ec4c9
withdrw <finesse@usa.com>
2024-07-11 15:55:14 -0500

    add aws

---
+++ app/forms/post_form.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Email, ValidationError
+from app.models import posts
+
+
+def minTitle(form, field):
+    if len(field.data) < 10:
+        raise ValidationError('Title must be at least 10 characters long')
+def minBody(form, field):
+    if len(field.data) < 150:
+        raise ValidationError('Body must be at least 150 characters long')
+
+
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), minTitle])
+    body = StringField("Body", validators=[DataRequired(), minBody])
+
+class PostUpdate(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(), minTitle])
+    body = StringField("Body", validators=[DataRequired(), minBody])
---
+++ app/models/posts.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String, nullable = False)
+    body = db.Column(db.String, nullable= False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')),nullable = False)
+    time_created = db.Column(db.DateTime(timezone = True),server_default = db.func.now())
+    post_image = db.Column(db.String, nullable = False)
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'title': self.title,
+            'body':self.body,
+            'userId': self.user_id,
+            'timeCreated': self.time_created,
+            'imageUrl': self.post_image
+        }
+
+
+class PostImage(db.Model):
+    __tablename__ = 'postImage'
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer,primary_key=True)
+    image_url = db.Column(db.String,nullable = False)
+    post_id = db.Column(db.Integer,db.ForeignKey(add_prefix_for_prod('post_id')),nullable = False)
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'imageUrl' : self.image_url,
+            'postId' : self.post_id
+        }
