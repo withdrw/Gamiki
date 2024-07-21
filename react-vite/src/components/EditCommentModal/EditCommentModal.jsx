31b7251b6bf7a4ad9bcf04a265246b36bddc5650
withdrw <finesse@usa.com>
2024-07-20 20:09:16 -0500

    last validation

--- react-vite/src/components/EditCommentModal/EditCommentModal.jsx
+++ react-vite/src/components/EditCommentModal/EditCommentModal.jsx
@@ -7,8 +7,21 @@ const EditCommentModal = ({ comment, onClose }) => {
   const [body, setBody] = useState(comment.body);
   const dispatch = useDispatch();
   const { closeModal } = useModal();
+  const [bodyTyped, setBodyTyped] = useState(body.length);
+  const [errors, setErrors] = useState('')
 
   const handleUpdate = async () => {
+
+    const errors = {};
+    if (body.length < 15 || body.length >= 450) {
+      errors.body = "Comment must be between 15 and 450 characters long";
+    }
+    if (Object.keys(errors).length > 0) {
+      setErrors(errors);
+      return;
+    }
+
+
     const updatedComment = { body: body };
     console.log("updated comment: ", updatedComment);
     console.log("comment id: ", comment.id);
@@ -23,8 +36,13 @@ const EditCommentModal = ({ comment, onClose }) => {
     }
     closeModal();
 
-
   };
+
+    const handleBodyChange = (e) => {
+      const value = e.target.value;
+      setBody(value);
+      setBodyTyped(value.length);
+    };
 
   return (
     <div className="modal">
@@ -33,9 +51,12 @@ const EditCommentModal = ({ comment, onClose }) => {
         <h2>Edit Comment</h2>
         <textarea
           value={body}
-          onChange={(e) => setBody(e.target.value)}
+          // onChange={(e) => setBody(e.target.value)}
+          onChange={handleBodyChange}
           placeholder="Body"
-        />
+        />{" "}
+        <p className="char-counter">{bodyTyped} characters count</p>
+        {errors.body && <p>{errors.body}</p>}
         <button onClick={handleUpdate}>Update</button>
       </div>
     </div>
